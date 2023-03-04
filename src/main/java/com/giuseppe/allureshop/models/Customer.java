8ed39e5f259851dfa19663b9625f3a12289dc02b
Giuseppe Desiato <giodesiato@gmail.com>
2023-03-04 08:58:58 -0500

    cutomer list

--- src/main/java/com/giuseppe/allureshop/models/Customer.java
+++ src/main/java/com/giuseppe/allureshop/models/Customer.java
@@ -21,6 +21,7 @@ public class Customer {
     private Long id;
     private String firstName;
     private String lastName;
+    private String userName;
     private String email;
     private String address;
 
