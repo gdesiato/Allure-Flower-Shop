46d7ffd1f62588e18e92abdd038eb774f8be924c
Giuseppe Desiato <giodesiato@gmail.com>
2023-04-02 08:59:09 -0400

    redis caching

--- src/main/java/com/giuseppe/allureshop/configuration/AppConfiguration.java
+++ src/main/java/com/giuseppe/allureshop/configuration/AppConfiguration.java
@@ -1,10 +1,12 @@
 package com.giuseppe.allureshop.configuration;
 
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@EnableCaching
 public class AppConfiguration {
 
     @Bean
--- src/main/java/com/giuseppe/allureshop/services/FlowerServiceImpl.java
+++ src/main/java/com/giuseppe/allureshop/services/FlowerServiceImpl.java
@@ -2,9 +2,11 @@ package com.giuseppe.allureshop.services;
 
 import com.giuseppe.allureshop.exceptions.FlowerNotFoundException;
 import com.giuseppe.allureshop.models.Flower;
-import com.giuseppe.allureshop.models.User;
 import com.giuseppe.allureshop.repositories.FlowerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,27 +20,27 @@ public class FlowerServiceImpl implements FlowerService {
 
 
     @Override
+    @Cacheable(value = "flowers", key = "'all'")
     public List<Flower> getAllFlowers() {
         return flowerRepository.findAll();
     }
 
-//    @Override
-//    public FlowerComposition createComposition(List<Long> flowerIds) {
-//        return null;
-//    }
 
     @Override
+    @Cacheable(value = "flowers", key = "#id")
     public Optional<Flower> getFlowerById(Long id) {
         return Optional.ofNullable(flowerRepository.findById(id)
                 .orElse(null));
     }
 
     @Override
+    @CacheEvict(value = "flowers", allEntries = true)
     public void deleteFlower(Long id) {
         flowerRepository.deleteById(id);
     }
 
     @Override
+    @CachePut(value = "flowers", key = "#id")
     public Flower updateFlower(Long id, Flower flower) {
         Optional<Flower> optionalFlower = flowerRepository.findById(id);
         if (optionalFlower.isPresent()) {
@@ -52,14 +54,9 @@ public class FlowerServiceImpl implements FlowerService {
     }
 
     @Override
+    @CachePut(value = "flowers", key = "#result.id")
     public Flower saveFlower(Flower flower) {
         return flowerRepository.save(flower);
     }
 
-
-    // method that takes in User as part of parameters
-
-//    public List<Flower> getFlowersForUser(User user){
-//
-//    }
 }
